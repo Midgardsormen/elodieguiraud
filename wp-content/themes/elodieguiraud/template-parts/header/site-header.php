0d89dad8346a2f46cbee220840879f0630de60dd
Midgardsormen <mdecourtray11@gmail.com>
2021-09-25 13:24:57 +0200

    feat(header): styling header

--- wp-content/themes/elodieguiraud/template-parts/header/site-header.php
+++ wp-content/themes/elodieguiraud/template-parts/header/site-header.php
@@ -14,13 +14,13 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 ?>
 
 <header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>" role="banner">
-<nav id="top-menu" class="top-menu">
-	<?php
-		wp_nav_menu( array( 
-			'theme_location' => 'my-custom-menu', 
-			'container_class' => 'custom-menu-class' ) ); 
-		?>
-</nav>
+	<nav id="top-menu" class="top-menu">
+		<?php
+			wp_nav_menu( array( 
+				'theme_location' => 'my-custom-menu', 
+				'container_class' => 'custom-menu-class' ) ); 
+			?>
+	</nav>
 	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 
 
